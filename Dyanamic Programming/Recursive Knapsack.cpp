88adb098191fdf62576ed0d6e3896cdbe1bbf4ec
Yash Chaudhari <chaudhariyash10@gmail.com>
2021-01-28 11:19:34 +0530

    Dynamic Programming Classic problems

---
+++ Dyanamic Programming/Recursive Knapsack.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int knapsack(int val[], int wt[], int W, int n)
+{
+    if (n == 0 || W == 0)
+        return 0;
+
+    if (wt[n - 1] > W)
+        return knapsack(val, wt, W, n - 1);
+
+    else
+        return max(val[n - 1] + knapsack(val, wt, W - wt[n - 1], n - 1), knapsack(val, wt, W, n - 1));
+}
+
+int main()
+{
+    int val[] = {1, 2, 4, 5, 6, 8};
+    int wt[] = {1, 2, 3, 5, 6, 7};
+
+    int W = 7;
+    
+    cout << knapsack(val, wt, W, 6);
+}
