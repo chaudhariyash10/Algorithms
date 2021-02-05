0f313e14c756a3ade90bb3d8e2c5a53fbc388f30
Yash Chaudhari <chaudhariyash10@gmail.com>
2021-02-05 10:48:41 +0530

    Unbounded Knapsack

---
+++ Dyanamic Programming/Unbounded Knapsack.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//n < 1000 && w < 1000
+int dp[1001][1001];
+
+int knapsack(int val[], int wt[], int W, int n)
+{
+    dp[0][0] = true;
+
+    for (int i = 1; i < 1001; i++)
+    {
+        dp[i][0] = true;
+        dp[0][i] = false;
+    }
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < W + 1; j++)
+        {
+            if (val[i - 1] > j)
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - val[i - 1]]);
+        }
+    }
+
+    return dp[n][W];
+}
+
+int main()
+{
+    int val[] = {1, 2, 4, 5, 4, 8};
+    int wt[] = {1, 2, 3, 5, 6, 7};
+    int n = 6;
+    int W = 6;
+
+    cout << knapsack(val, wt, W, n);
+}
